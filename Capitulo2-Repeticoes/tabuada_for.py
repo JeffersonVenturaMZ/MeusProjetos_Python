290c8ec0341d7fa0e0cf2f2e7b9e1296dcb2f60f
Jefferson JV. Ventura <jeffersonventura@gac.local>
2021-12-03 14:55:27 -0300

    organização de commit

--- Capitulo2-Repeticoes/tabuada_for.py
+++
@@ -1,7 +0,0 @@
-tabuada=int(input("Digite um número para exibir a tabuada: "))
-print("Tabuada do número ", tabuada)
-for valor in range(1,11,1):
-    print(str(tabuada) + " x " + str(valor) + " = " + str((tabuada*valor)))
-
-# No range(), definimos que os valores gerados deverão estar entre 1 e 11, com incremento de 1 em 1.
-# O “11” foi definido porque ele não se inclui no range, ou seja, quando chegar em 11 ele para, portanto, não executará mais o laço quando atingir este valor.    
